cfb53bb0dacc3e5e8d5774f0a93ec3e0c70b95fd
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-08 21:09:20 +0900

    Add : (Lv.0)문자 리스트를 문자열로 변환하기 추가 test : 레벨별 폴더링

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181941.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//문자 리스트를 문자열로 변환하기
+public class Solution_181941
+{
+    public string solution(string[] arr)
+    {
+        string answer = "";
+        foreach (string word in arr)
+        {
+            answer += word;
+        }
+        return answer;
+    }
+}
