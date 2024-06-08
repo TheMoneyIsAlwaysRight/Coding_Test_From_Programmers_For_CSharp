6148f24d0e2782495e228b8330271a24b195267b
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-08 21:56:37 +0900

    ADD : MEMO.cs 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/MEMO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 새롭게 알게 된 함수 혹은 잊어먹기 쉬운 함수 기록
+
+/*
+    Substring
+    Equals
+    string.StartsWith
+    IndexOf
+ToLower()
+
+Array.Resize
+List.Any();
+Convert.ToInt32
+
+ */
+
+public class MEMO
+{
+
+}
+
