20d26fe3a907ffd2cba26b7e1c77aff9671f0182
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-08 21:43:52 +0900

    Add : Lv.0 : 접두사인지 확인하기 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181906.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//Lv.0 : 접두사인지 확인하기.
+public class Solution_181906
+{
+    public int solution(string my_string, string is_prefix)
+    {
+        char[] array = my_string.ToCharArray();
+        List<string> list = new List<string>();
+
+        for (int x = 0; x < my_string.Length; x++)
+        {
+            string word = "";
+            for (int y = 0; y <= x; y++)
+            {
+                word += array[y];
+            }
+            if (word != "")
+            {
+                list.Add(word);
+            }
+        }
+        if (list.Contains(is_prefix))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
