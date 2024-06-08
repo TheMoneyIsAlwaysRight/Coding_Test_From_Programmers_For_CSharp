50204aeaafc352acbf7f7f77091154265382f7a0
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-08 21:34:57 +0900

    Add : Lv.0 : 배열 만들기 1 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181901.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//Lv.0 : 배열 만들기 1
+public class Solution_181901
+{
+    public int[] solution(int n, int k)
+    {
+        List<int> list = new List<int>();
+
+        for (int x = 1; x <= n; x++)
+        {
+            if (x % k == 0)
+            {
+                list.Add(x);
+            }
+
+        }
+        int[] answer = list.ToArray();
+        return answer;
+    }
+}
