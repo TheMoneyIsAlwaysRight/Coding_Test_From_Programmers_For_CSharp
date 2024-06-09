132ce45c913f75b5edef80ec9b5205270b5e9448
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-09 11:01:26 +0900

    comment : MEMO 함수 이름 정리

--- Coding_Test_From_Programmers_For_CSharp/MEMO.cs
+++ Coding_Test_From_Programmers_For_CSharp/MEMO.cs
@@ -7,32 +7,28 @@ using System.Threading.Tasks;
 // 새롭게 알게 된 함수 혹은 잊어먹기 쉬운 함수 기록
 
 /*
+    <<System>> ***********************************
     Substring
     Equals
-    string.StartsWith
-    IndexOf
+    String.StartsWith
+    Array.IndexOf
     ToLower()
     Array.Resize
-    List.Any();
     Convert.ToInt32
-string. Concat();
-string.IsNullOrEmpty
-string.Distinct() 메소드 ---> 링큐 연관됨
-using System.Linq; 써야한다.
+    String.Concat
+    String.IsNullOrEmpty(String)
+    String.Replace
+    Int32.TryParse
 
-- ToArray(): Linq 결과를 배열로 변환합니다.
-
-- ToList(): Linq 결과를 List로 변환합니다.
-
-
-string.Substring(int32) : 인스턴스 메소드
-Substring 메서드는 문자열의 특정위치부터 특정길이까지의 문자열을 반환하는 메서드를 말합니다
-STR.Substinrg(0,3) -> 0인데스에서 3인덱스까지 문자열 반환.
+    <<System.Linq>> ******************************
+    Enumerable.Any
+    Enumerable.ToList<TSource>
+    Enumerable.Distinct
 
 
-string.Count
-널 조건 연산자 : !
-<List> Contain 파트
+    <<System.Collections.Generic>>****************
+    List<T>.ToArray
+    Enumerable.ToList<TSource>
 
  */
 
