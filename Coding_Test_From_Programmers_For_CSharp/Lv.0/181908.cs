95ae8a0a498e2a938d11968bfec24089f8b08b4d
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-09 09:47:47 +0900

    Add : Lv.0 : 조건 문자열 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181908.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+//Lv.0 : 접미사인지 확인하기 ----------> 덜품
+
+
+public class Solution_181908
+{
+    public int solution(string my_string, string is_suffix)
+    {
+        List<string> list = new List<string>();
+
+        for (int x = 0; x < my_string.Length; x++)
+        {
+            string word = "";
+            for (int y = 0; y <= x; y++)
+            {
+                word += my_string[my_string.Length - (x + 1)];
+            }
+            if (word != "")
+            {
+                list.Add(word);
+            }
+        }
+        if (list.Contains(is_suffix)) { return 1; }
+
+        return 0;
+    }
+    public static void Main()
+    {
+        Solution_181908 instance = new Solution_181908();
+
+        int result = instance.solution($"banana", "ana");
+
+        Console.Write(result);
+    }
+}
---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181934.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+//Lv.0 : 조건 문자열
+public class Solution_181934
+{
+    public int solution(string ineq, string eq, int n, int m)
+    {
+        int result;
+        if (ineq == "<")
+        {
+            if (eq == "=")
+            {
+                result = (n <= m) ? 1 : 0;
+            }
+            else
+            {
+                result = (n < m) ? 1 : 0;
+            }
+        }
+        else
+        {
+
+            if (eq == "=")
+            {
+                result = (n >= m) ? 1 : 0;
+            }
+            else
+            {
+                result = (n > m) ? 1 : 0;
+            }
+        }
+
+        return result;
+    }
+}
