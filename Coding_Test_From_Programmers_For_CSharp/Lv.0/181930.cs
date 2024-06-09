cf6927bacb6715aa86a2e2b3cbfce57dd0b6b183
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-09 10:40:31 +0900

    Add : Lv 0 : 주사위 게임 2

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181930.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+//Lv 0 : 주사위 게임 2
+public class Solution_181930
+{
+    public int solution(int a, int b, int c)
+    {
+        double result;
+
+        if ((a != b) && (b != c) && (a != c))
+        {
+            result = a + b + c;
+        }
+        else if ((a == b) && (b == c))
+        {
+            result = (a + b + c) * (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2)) * (Math.Pow(a, 3) + Math.Pow(b, 3) + Math.Pow(c, 3));
+        }
+        else
+        {
+
+            result = (a + b + c) * (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2));
+        }
+
+        return (int)result;
+
+    }
+}
