f296d05bcd5df4545e6424c0b513de32ae2ebe39
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-10 21:30:24 +0900

    Add : Lv.0 : 마지막 두 원소 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/120812.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution_120812
+{
+    public int solution(int[] array)
+    {
+        List<int> list = Enumerable.Distinct(array).ToList<int>();
+        List<int> count_list = new List<int>();
+
+        foreach (int x in list)
+        {
+            int count = 1;
+            for (int y = 0; y < array.Length; y++)
+            {
+                if (x.Equals(array[y])) { count++; }
+            }
+            count_list.Add(count);
+        }
+        int maxValue = count_list.Max();
+        int max_index = count_list.IndexOf(maxValue); //최빈값이 있는 인덱스
+
+        if ((count_list.IndexOf(maxValue, max_index + 1) == -1)) { return maxValue; } //최빈값으로 기록된 값이 하나 뿐일때
+
+        return -1;
+    }
+    public static void Main()
+    {
+        Solution_120812 instance = new Solution_120812();
+        int[] sibal = { 1, 2, 3, 3, 3, 4 };
+        int result = instance.solution(sibal);
+        Console.WriteLine(result);
+    }
+}
---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181927.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// Lv.0 : 마지막 두 원소
+public class Solution_181927
+{
+    public int[] solution(int[] num_list)
+    {
+        List<int> list = num_list.ToList<int>();
+        int[] result;
+
+        if (num_list[num_list.Length - 1] > num_list[num_list.Length - 2])
+        {
+            list.Add(num_list[num_list.Length - 1] - num_list[num_list.Length - 2]);
+        }
+        else
+        {
+            list.Add(2 * num_list[num_list.Length - 1]);
+        }
+
+        result = list.ToArray<int>();
+
+        return result;
+    }
+}
