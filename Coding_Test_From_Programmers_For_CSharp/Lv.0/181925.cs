10b363cad4c25d7e05115f5c0f31b2559c09b65a
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-10 21:46:41 +0900

    Add : Lv.0 : 수 조작하기 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181925.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+//Lv.0 : 수 조작하기
+public class Solution
+{
+    public string solution(int[] numLog)
+    {
+        StringBuilder result = new StringBuilder();  //stringBuilder (x) -> StringBuilder (O)
+
+        int prevNumber = numLog[0];
+
+        foreach (int x in numLog)
+        {
+            if (x == prevNumber) { continue; }
+
+            if (x != prevNumber)
+            {
+                int y = Math.Abs(x - prevNumber);
+
+                switch (y)
+                {
+                    case 1:
+                        if (x > prevNumber) { result.Append("w"); }
+                        else { result.Append("s"); }
+                        break;
+                    case 10:
+                        if (x > prevNumber) { result.Append("d"); }
+                        else { result.Append("a"); }
+                        break;
+                }
+
+                prevNumber = x;
+            }
+
+        }
+
+        return result.ToString();
+
+    }
+}
