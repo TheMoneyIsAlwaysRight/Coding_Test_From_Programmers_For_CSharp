dc7aee2e68dd4cf498c1563212b7bdc3177c3ed2
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-14 22:46:28 +0900

    ADD : Lv.0 : 주사위 게임 1 추가

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181839.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//Lv.0 : 주사위 게임 1
+public class Solution_181839
+{
+    public int solution(int a, int b)
+    {
+        if (a % 2 != 0 && b % 2 != 0) { return (int)(Math.Pow(a, 2) + Math.Pow(b, 2)); }
+        else if (a % 2 != 0 && b % 2 == 0) { return 2 * (a + b); }
+        else if (a % 2 == 0 && b % 2 != 0) { return 2 * (a + b); }
+        else { return Math.Abs(a - b); }
+    }
+}
--- Coding_Test_From_Programmers_For_CSharp/MEMO.cs
+++ Coding_Test_From_Programmers_For_CSharp/MEMO.cs
@@ -14,24 +14,39 @@ using System.Threading.Tasks;
  * 
 ************************************** <<System>> ********************************************************************************
 
+    -문자열 관련-
 
                 String.Substring(int index) : 문자열의 특정 인덱스부터 문자열 끝까지의 부분문자열 추출 -> string형으로 반환.
                 -> instance.Substring(start_Index);
 
                 String.Substring(int start_index ,int Length)  : 문자열의 특정 인덱스부터 특정길이 만큼의 부분문자열 추출 -> string형으로 반환.
                 -> instance.Substring(start_Index,Length);
-    
-                Object.Equals(Object) : 이 객체와 매개변수의 객체가 같은지 검사 -> bool형 반환.
-                -> instance.Equals(other_instance);
-    
-   <Static>     Object.Equals(Object,Object) : 매개변수의 두 객체가 같은지 검사 -> bool형 반환.
-                -> ex : Object.Equals(IsEqual, IsEquals);
+
+                String.StartsWith(string word): 문자열이 이 문자열로 시작하는지 검사 -> bool형 반환.
+                -> instance.StartWith("word");
+
+                String.Replace(char old_value,char new_Value) : 문자열의 모든 old_value 문자를 new_Value 문자로 바꾼 문자열 반환 -> string형 반환
+                ->instance.Replace(old_value,new_Value);
+
+                String.Replace(String old_value,String new_Value) : 문자열의 모든 old_value 문자열을 new_Value 문자열로 바꾼 문자열 반환 -> string형 반환
+                ->instance.Replace(old_value,new_Value);
+
+                String.ToLower() : 문자열의 모든 대문자를 소문자로 바꾼 새 문자열 생성 -> string형 반환
+                -> instance.ToLower();
+
+                String.ToUpper() : 문자열의 모든 소문자를 대문자로 바꾼 새 문자열 생성 -> string형 반환 
+                -> instance.ToUpper();
+
+   <Static>     String.Concat(params string[] value) : 매개변수의 문자열들 혹은 문자열 배열의 모든 요소들을 연결한 새로운 문자열 생성, params 연산자 덕분에 매개변수 제한 X -> string형 반환
+                ->String.Concat("Hello","World");
+
+   <Static>     String.IsNullOrEmpty(String value) : 매개변수의 문자열이 빈 문자열("")이거나, null인지 검사하여, 전자일 경우 True, 아닐경우 false 반환.
+                ->String.IsNullorEmpty(word);
 
 <High_Version>  String.StartsWith(char letter): 문자열이 이 문자로 시작하는지 검사 -> bool형 반환.
                 -> instance.StartsWith('s'); 
 
-                String.StartsWith(string word): 문자열이 이 문자열로 시작하는지 검사 -> bool형 반환.
-                -> instance.StartWith("word");
+    -배열 관련-
 
    <Static>     Array.Sort(Array arrayName) : IComparable 구현된 각 요소의 CompareTo 메서드를 통해 정렬 알고리즘으로 그 배열을 재정렬.
                 -> Array.Sort(arrayName);
@@ -45,14 +60,10 @@ using System.Threading.Tasks;
    <Static>     Array.Reverse(Array arrayName) : 배열의 모든 요소를 역순으로 뒤집는다.        
                 ->Array.Reverse(arrayName);
 
-                String.ToLower() : 문자열의 모든 대문자를 소문자로 바꾼 새 문자열 생성 -> string형 반환
-                -> instance.ToLower();
-
-                String.ToUpper() : 문자열의 모든 소문자를 대문자로 바꾼 새 문자열 생성 -> string형 반환 
-                -> instance.ToUpper();
-
    <Static>     Array.Resize<T>(ref Array<T> arrayName,int size) : 그 배열을 참조해서(ref) 크기를 size만큼으로 조절. size보다 큰 index에 있던 데이터들은 전부 삭제됨.
                 ->Array.Resize(ref arrayName,size);
+
+    -변환 관련-
                                
    <Static>     Convert.ToString(int value,int number) : 주어진 값을 문자열로 변환할 건데, 먼저 값을 number(2,8,10,16)진법으로 변환하고 이를 문자열로 반환한다.
                 ->Convert.ToString(value,Number);
@@ -66,20 +77,16 @@ using System.Threading.Tasks;
    <Static>     Convert.ToDouble(Object value) : 매개변수의 객체을 double형으로 변환 -> double형 반환.
                 ->Convert.ToDouble(value);
 
-   <Static>     String.Concat(params string[] value) : 매개변수의 문자열들 혹은 문자열 배열의 모든 요소들을 연결한 새로운 문자열 생성, params 연산자 덕분에 매개변수 제한 X -> string형 반환
-                ->String.Concat("Hello","World");
-
-   <Static>     String.IsNullOrEmpty(String value) : 매개변수의 문자열이 빈 문자열("")이거나, null인지 검사하여, 전자일 경우 True, 아닐경우 false 반환.
-                ->String.IsNullorEmpty(word);
-
-                String.Replace(char old_value,char new_Value) : 문자열의 모든 old_value 문자를 new_Value 문자로 바꾼 문자열 반환 -> string형 반환
-                ->instance.Replace(old_value,new_Value);
-
-                String.Replace(String old_value,String new_Value) : 문자열의 모든 old_value 문자열을 new_Value 문자열로 바꾼 문자열 반환 -> string형 반환
-                ->instance.Replace(old_value,new_Value);
-
    <Static>     Int32.TryParse(String value,out int result) : 첫번째 매개변수의 문자열을 int형으로 변환을 시도하고, 성공유무에 따라 bool형으로 반환, 성공시 out 파라미터의 result에 값 반환. 실패시 result는 0 반환.
                 ->Int32.TryParse(instance,out int result)
+
+    -객체 관련-
+                Object.Equals(Object) : 이 객체와 매개변수의 객체가 같은지 검사 -> bool형 반환.
+                -> instance.Equals(other_instance);
+    
+   <Static>     Object.Equals(Object,Object) : 매개변수의 두 객체가 같은지 검사 -> bool형 반환.
+                -> ex : Object.Equals(IsEqual, IsEquals);
+
 
 
 
@@ -105,7 +112,7 @@ using System.Threading.Tasks;
                 -> Dictionary.FirstOrDefault(item, item => item.GetComponent().IsClear) => 딕셔너리 요소탐색에 람다식 사용가능
 
 
-************************************************ <<System.Collections.Generic>> ************************************************************************************************
+****************************************<<System.Collections.Generic>> ************************************************************************************************
 
 
                 List<T>.ToArray() : 컬렉션의 요소들을 새 배열에 복사하고 그 배열을 반환. ->   T[]형 반환.
