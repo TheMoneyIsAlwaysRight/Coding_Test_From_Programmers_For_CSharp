6f799d0ebeb0b91f6131cc988a00bb48ec955564
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-06-19 22:29:00 +0900

    Add : 안 풀린 181923,18841 문제 추가 --> 반례 찾아야함(런타임 에러)

---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181841.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+//반례찾기
+public class Solution_181841
+{
+    public string solution(string[] str_list, string ex)
+    {
+        StringBuilder result = new StringBuilder();
+        for (int x = 0; x < str_list.Length; x++)
+        {
+            string subWord = str_list[x].Substring((str_list.Length - ex.Length));
+            if (subWord != ex)
+            {
+                result.Append(str_list[x]);
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ Coding_Test_From_Programmers_For_CSharp/Lv.0/181923.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//반례 찾아야함
+public class Solution_181923
+{
+    public int[] solution(int[] arr, int[,] queries)
+    {
+        List<int> backUp = new List<int>();
+        List<int> querie_List = new List<int>();
+        List<int> result = new List<int>();
+
+        for (int x = 0; x < queries.GetLength(1); x++)
+        {
+            for (int y = 0; y < queries.GetLength(0); y++)
+            {
+                querie_List.Add(queries[x, y]);
+            }
+
+            for (int z = querie_List[0]; z <= querie_List[1]; z++)
+            {
+                backUp.Add(arr[z]);
+            }
+            if (backUp.Exists(item => item > querie_List[2]))
+            {
+                backUp.Sort();
+                result.Add(backUp.FirstOrDefault(item => item > querie_List[2]));
+                backUp.Clear();
+                querie_List.Clear();
+            }
+            else
+            {
+                result.Add(-1);
+                backUp.Clear();
+                querie_List.Clear();
+            }
+
+        }
+        return result.ToArray();
+    }
+}
